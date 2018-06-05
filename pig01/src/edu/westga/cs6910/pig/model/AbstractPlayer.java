d60d4622beb721e5b45c14a310c5bd74015a8434
Drew Coleman <dcolem12@my.westga.edu>
2018-06-05 00:25:23 -0400

    Re-added the Override to all the methods within the AbstractPlayer so thye will match up with the Player interface

--- pig01/src/edu/westga/cs6910/pig/model/AbstractPlayer.java
+++ pig01/src/edu/westga/cs6910/pig/model/AbstractPlayer.java
@@ -1,7 +1,9 @@
 package edu.westga.cs6910.pig.model;
 
 /**
- * This is an Abstract Class which will provide the building blocks for the Player classes
+ * This is an Abstract Class which will provide the building blocks for the
+ * Player classes
+ * 
  * @author Drew Coleman
  * @version 06/05/2018
  *
@@ -9,12 +11,53 @@ package edu.westga.cs6910.pig.model;
 public abstract class AbstractPlayer implements Player {
 	private String name;
 	private DicePair thePair;
-	private boolean isMyTurn;	
+	private boolean isMyTurn;
 	private int total;
 	private int turnTotal;
-	
+
 	AbstractPlayer(String name) {
-		
+		this.name = name;
+		this.thePair = new DicePair();
+
 	}
 
+	@Override	
+	/**
+	 * @see Player#getIsMyTurn()
+	 */
+	public boolean getIsMyTurn() {
+		return this.isMyTurn;
+	}
+
+	@Override	
+	/**
+	 * @see Player#getTurnTotal()
+	 */
+	public int getTurnTotal() {
+		return this.turnTotal;
+	}
+
+	@Override	
+	/**
+	 * @see Player#getName()
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	@Override	
+	/**
+	 * @see Player#getDiceValues()
+	 */
+	public String getDiceValues() {
+		return this.thePair.getDie1Value() + ", " + this.thePair.getDie2Value();
+	}
+
+	@Override	
+	/**
+	 * @see Player#getTotal()
+	 */
+	public int getTotal() {
+		return this.total;
+	}
 }
