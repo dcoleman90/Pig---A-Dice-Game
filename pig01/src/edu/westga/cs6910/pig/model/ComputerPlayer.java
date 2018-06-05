afc877d234fefce9b287e6c41abf9d3b8e94f655
Drew Coleman <dcolem12@my.westga.edu>
2018-06-05 12:09:36 -0400

    Code greatly cleaned up all unneeded comments inside ComputerPlayer

--- pig01/src/edu/westga/cs6910/pig/model/ComputerPlayer.java
+++ pig01/src/edu/westga/cs6910/pig/model/ComputerPlayer.java
@@ -1,13 +1,5 @@
 package edu.westga.cs6910.pig.model;
 
-// TODO: Classes ComputerPlayer and HumanPlayer share most of their code.
-//		 Refactor their code:
-// 		 1. Create abstract base class AbstractPlayer to implement the
-//			shared code and define abstract methods for methods without
-//			duplicate code. AbstractPlayer should implement interface Player.
-//		 2. Have ComputerPlayer and HumanPlayer extend AbstractPlayer to
-//		    implement the unshared constructor code and the abstract methods.
-
 /**
  * ComputerPlayer represents a very simple automated player in the game Pig.
  * It rolls exactly 1 time and then holds.
@@ -15,15 +7,11 @@ package edu.westga.cs6910.pig.model;
  * @author	CS6910
  * @version	Summer 2018
  */
-public class ComputerPlayer extends AbstractPlayer{
+public class ComputerPlayer extends AbstractPlayer {
 	
 	private static final String NAME = "Simple computer";
 	
 	private String name;
-//	private DicePair thePair;
-//	private boolean isMyTurn;
-//	private int total;
-//	private int turnTotal;
 	private int maximumRolls;
 	
 	/**
@@ -36,13 +24,6 @@ public class ComputerPlayer extends AbstractPlayer{
 	}
 
 	//*************************** mutator methods ****************************
-//	@Override
-//	/**
-//	 * @see Player#resetTurnTotal()
-//	 */	
-//	public void resetTurnTotal() {
-//		this.turnTotal = 0;
-//	}
 	
 	/**
 	 * Implements Player's setMaximumRolls, but is not normally
@@ -76,38 +57,8 @@ public class ComputerPlayer extends AbstractPlayer{
 			counter++;
 		}
 	}
-	
-//	@Override
-//	/**
-//	 * @see Player#takeTurn()
-//	 */	
-//	public void takeTurn() {				
-//		for (int count = 0; count < this.maximumRolls; count++) {
-//			this.thePair.rollDice();
-//			
-//			int die1Value = this.thePair.getDie1Value();
-//			int die2Value = this.thePair.getDie2Value();
-//			if (die1Value == 1 || die2Value == 1) {
-//			
-//				this.total -= this.turnTotal;
-//				this.isMyTurn = false;
-//				return;
-//			} else {		
-//				this.turnTotal += die1Value + die2Value;
-//				this.total += die1Value + die2Value;		
-//			}		
-//		}
-//		this.isMyTurn = false;
-//	}
 
 	//*************************** accessor methods ****************************
-//	@Override	
-//	/**
-//	 * @see Player#getIsMyTurn()
-//	 */
-//	public boolean getIsMyTurn() {
-//		return this.isMyTurn;
-//	}
 	
 	@Override	
 	/**
@@ -116,28 +67,4 @@ public class ComputerPlayer extends AbstractPlayer{
 	public String getName() {
 		return this.name;
 	}
-	
-//	@Override	
-//	/**
-//	 * @see Player#getTurnTotal()
-//	 */
-//	public int getTurnTotal() {
-//		return this.turnTotal;
-//	}
-//	
-//	@Override	
-//	/**
-//	 * @see Player#getTotal()
-//	 */
-//	public int getTotal() {
-//		return this.total;
-//	}	
-//	
-//	@Override	
-//	/**
-//	 * @see Player#getDiceValues()
-//	 */
-//	public String getDiceValues() {	
-//		return this.thePair.getDie1Value() + ", " + this.thePair.getDie2Value();
-//	}
 }
