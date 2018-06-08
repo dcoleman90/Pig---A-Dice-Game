1e1dad5c466381091e56a5ac6505472721d65ef9
Drew Coleman <dcolem12@my.westga.edu>
2018-06-08 15:03:17 -0400

    Added test to insure that the computer player total starts at 0 when the class is constructed

---
+++ pig01/src/edu/westga/cs6910/testing/computerPlayer/TestWhenGetTotal.java
@@ -0,0 +1,19 @@
+package edu.westga.cs6910.testing.computerPlayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+
+class TestWhenGetTotal {
+
+	/**
+	 * This test will insure when the class is constructed it returns a total of 0;
+	 */
+	@Test
+	void testConstructedTotalResultsIn0() {
+		ComputerPlayer acer = new ComputerPlayer();
+		assertEquals(0, acer.getTotal());
+	}
+
+}
