41c242b5a0793850049b58a006e0bda8688b2ced
Drew Coleman <dcolem12@my.westga.edu>
2018-06-07 23:37:45 -0400

    Added Test to insure the getNumberOfPips returned one when a new die was created
    Test returned successfully

---
+++ pig01/src/edu/westga/cs6910/pig/testing/die/TestWhenGetPips.java
@@ -0,0 +1,25 @@
+package edu.westga.cs6910.pig.testing.die;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.Die;
+
+class TestWhenGetPips {
+
+	/**
+	 * This test will check the getNumberOfPips method and insure it returns one
+	 * when a new die is called
+	 */
+	@Test
+	void testWithNewDieShouldHave1Pip() {
+		Die newDie = new Die(new Random());
+		assertEquals(1, newDie.getNumberOfPips());
+	}
+	
+	
+
+}
