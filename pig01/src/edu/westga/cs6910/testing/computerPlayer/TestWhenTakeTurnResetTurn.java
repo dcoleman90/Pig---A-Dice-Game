bf93db0c59e9c0b7a2cd2ee9bb1cd8e919069443
Drew Coleman <dcolem12@my.westga.edu>
2018-06-08 16:53:27 -0400

    Corrected lack of code to make the test functional

--- pig01/src/edu/westga/cs6910/testing/computerPlayer/TestWhenTakeTurnResetTurn.java
+++ pig01/src/edu/westga/cs6910/testing/computerPlayer/TestWhenTakeTurnResetTurn.java
@@ -1,9 +1,11 @@
 package edu.westga.cs6910.testing.computerPlayer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
 
 class TestWhenTakeTurnResetTurn {
 
@@ -11,9 +13,21 @@ class TestWhenTakeTurnResetTurn {
 	void tearDown() throws Exception {
 	}
 
+	/**
+	 * This test will insure that the takeTurn method successfully resets the turn
+	 * total to 0 It test this by calling the method
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testParametersOfTakeTurnByInsuringResetTurnTotalIsCalledWhenA1IsRolled() {
+		ComputerPlayer drew = new ComputerPlayer();
+		for (int count = 0; count < 1000; count++) {
+			drew.takeTurn();
+			if (drew.getDie1Result() == 1 || drew.getDie2Result() == 1) {
+				while (drew.getTurnTotal() != 0) {
+					fail("Test the computer players turn after a one was rolled was not equal to zero| Turn was equal to: " + drew.getTurnTotal());
+				}
+			}
+		}
 	}
 
 }
