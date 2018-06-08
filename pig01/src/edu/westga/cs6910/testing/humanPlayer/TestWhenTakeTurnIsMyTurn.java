da9d0d20ba752852f7ebde8702c160cf3dc13ae7
Drew Coleman <dcolem12@my.westga.edu>
2018-06-08 16:41:52 -0400

    Updated test to reflected the expected outcome for rolls with and without a one in the roll
    Test returned the expected results

--- pig01/src/edu/westga/cs6910/testing/humanPlayer/TestWhenTakeTurnIsMyTurn.java
+++ pig01/src/edu/westga/cs6910/testing/humanPlayer/TestWhenTakeTurnIsMyTurn.java
@@ -14,17 +14,35 @@ class TestWhenTakeTurnIsMyTurn {
 	}
 
 	/**
-	 * This test will insure that the takeTurn method successfully resets the turn
-	 * total to 0 It test this by calling the method
+	 * This test will insure that the takeTurn method successfully sets the isMyTurn instance variable to false
+	 * It test this by calling the method getIsMyTurn
 	 */
 	@Test
-	void testParametersOfTakeTurnByInsuringResetTurnTotalIsCalledWhenA1IsRolled() {
+	void testParametersOfTakeTurnByInsuringIsMyTurnEqualsFalseWhenA1IsRolled() {
 		HumanPlayer drew = new HumanPlayer("Drew");
 		for (int count = 0; count < 1000; count++) {
 			drew.takeTurn();
 			if (drew.getDie1Result() == 1 || drew.getDie2Result() == 1) {
 				while (drew.getIsMyTurn()) {
-					fail("Test remained the human players turn after a one was rolled: Die 1: " + drew.getDie1Result()
+					fail("Test remained true after a one was rolled: Die 1: " + drew.getDie1Result()
+							+ " Die 2: " + drew.getDie2Result());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * This test will insure that the takeTurn method successfully sets the isMyTurn instance variable to true
+	 * It test this by calling the method getIsMyTurn
+	 */
+	@Test
+	void testParametersOfTakeTurnByInsuringIsMyTurnEqualsTrueIsCalledWhenNo1sAreRolled() {
+		HumanPlayer drew = new HumanPlayer("Drew");
+		for (int count = 0; count < 1000; count++) {
+			drew.takeTurn();
+			if (drew.getDie1Result() != 1 && drew.getDie2Result() != 1) {
+				while (!drew.getIsMyTurn()) {
+					fail("Test remained false when numbers other than one where rolled: Die 1: " + drew.getDie1Result()
 							+ " Die 2: " + drew.getDie2Result());
 				}
 			}
