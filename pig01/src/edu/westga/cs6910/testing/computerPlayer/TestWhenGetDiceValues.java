84bc01d5794d5363c4bcadfa1af8d320eed3c79f
Drew Coleman <dcolem12@my.westga.edu>
2018-06-08 15:00:58 -0400

    Added test to insure that the DiceValues are set at 1 when initially called

---
+++ pig01/src/edu/westga/cs6910/testing/computerPlayer/TestWhenGetDiceValues.java
@@ -0,0 +1,21 @@
+package edu.westga.cs6910.testing.computerPlayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+
+class TestWhenGetDiceValues {
+
+	/**
+	 * This test will insure that when constructed the Die pips on both dice are set at 1
+	 * 
+	 */
+	@Test
+	void testGetDiceValuesIsCalledAfterConstructionBothDiceAreSetAt1() {
+		ComputerPlayer dell = new ComputerPlayer();
+		assertEquals("1, 1", dell.getDiceValues());
+	}
+
+}
