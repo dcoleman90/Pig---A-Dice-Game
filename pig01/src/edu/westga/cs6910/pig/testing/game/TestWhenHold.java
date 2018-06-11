6c11daec3981b40fab3b399c223be2dd15e2a31d
Drew Coleman <dcolem12@my.westga.edu>
2018-06-10 23:04:52 -0400

    Added test for hold()
    Tested to insure that the currentPlayer does change from computer to
    human and that the turnTotal resets to 0

---
+++ pig01/src/edu/westga/cs6910/pig/testing/game/TestWhenHold.java
@@ -0,0 +1,39 @@
+package edu.westga.cs6910.pig.testing.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+class TestWhenHold {
+
+	/**
+	 * This test checks to insure that when hold() is called the currentPlayer changes to the other player
+	 * In this test it changes from ComputerPlayer to HumanPlayer
+	 */
+	@Test
+	void testHoldCurrentPlayerSwitchesPlayerExpectedHumanPlayer() {
+		ComputerPlayer dell = new ComputerPlayer();
+		HumanPlayer wayne = new HumanPlayer("Wayne");
+		Game risk = new Game(wayne, dell);
+		risk.setTheComputer(dell);
+		risk.hold();
+		assertEquals(wayne, risk.getCurrentPlayer());
+	}
+	
+	/**
+	 * This test will insure that when hold() is called the resetTurnTotal is triggered and the new currentPlayer has a 
+	 * turn total of 0
+	 */
+	void testHoldCurrentPlayerResetTurnTotalTo0() {
+		ComputerPlayer dell = new ComputerPlayer();
+		HumanPlayer wayne = new HumanPlayer("Wayne");
+		Game risk = new Game(wayne, dell);
+		risk.setTheComputer(dell);
+		risk.hold();
+		assertEquals(0, wayne.getTurnTotal());
+	}
+}
