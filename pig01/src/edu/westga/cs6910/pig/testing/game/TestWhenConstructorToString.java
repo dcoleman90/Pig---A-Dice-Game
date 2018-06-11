fb199a91e8cd5e7c3907c792788278fad9d68535
Drew Coleman <dcolem12@my.westga.edu>
2018-06-10 22:54:43 -0400

    Added more testing to TestWhenConstructorToString checking to insure when values or win conditions are added the test display the expected results

--- pig01/src/edu/westga/cs6910/pig/testing/game/TestWhenConstructorToString.java
+++ pig01/src/edu/westga/cs6910/pig/testing/game/TestWhenConstructorToString.java
@@ -9,18 +9,56 @@ import edu.westga.cs6910.pig.model.Game;
 import edu.westga.cs6910.pig.model.HumanPlayer;
 
 class TestWhenConstructorToString {
-	
+
 	/**
-	 * This test will insure that after construction Game toString() displays the expected results
+	 * This test will insure that after construction Game toString() displays the
+	 * expected default results 
+	 * Goal Score: 100 
+	 * Wayne: 0 
+	 * Simple Computer: 0
+	 */
+	@Test
+	void testConstructorToStringByComparingToExpectedDefaultResult() {
+		ComputerPlayer dell = new ComputerPlayer();
+		HumanPlayer wayne = new HumanPlayer("Wayne");
+		Game risk = new Game(wayne, dell);
+		assertEquals("Goal Score: 100" + System.getProperty("line.separator") + "Wayne: 0"
+				+ System.getProperty("line.separator") + "Simple Computer: 0", risk.toString());
+	}
+
+	/**
+	 * This test will insure that after construction Game toString() displays the
+	 * expected results
+	 * Goal Score: 100 
+	 * Wayne: 99 
+	 * Simple Computer: 0
 	 */
 	@Test
 	void testConstructorToStringByComparingToExpectedResult() {
 		ComputerPlayer dell = new ComputerPlayer();
 		HumanPlayer wayne = new HumanPlayer("Wayne");
 		Game risk = new Game(wayne, dell);
-		assertEquals("Goal Score: 100"
-				+ System.getProperty("line.separator") + "Wayne: 0"
+		wayne.setTotal(99);
+		assertEquals("Goal Score: 100" + System.getProperty("line.separator") + "Wayne: 99"
 				+ System.getProperty("line.separator") + "Simple Computer: 0", risk.toString());
 	}
 	
+	/**
+	 * This test will insure that after construction Game toString() displays the
+	 * expected results
+	 * Goal Score: 100 
+	 * Wayne: 99 
+	 * Simple Computer: 0
+	 * Game over! Winner: Wayne
+	 */
+	@Test
+	void testConstructorToStringByComparingToExpectedResultWhenAPlayerWins() {
+		ComputerPlayer dell = new ComputerPlayer();
+		HumanPlayer wayne = new HumanPlayer("Wayne");
+		Game risk = new Game(wayne, dell);
+		wayne.setTotal(109);
+		assertEquals("Goal Score: 100" + System.getProperty("line.separator") + "Wayne: 109"
+				+ System.getProperty("line.separator") + "Simple Computer: 0" + System.getProperty("line.separator") 
+				+ "Game over! Winner: Wayne", risk.toString());
+	}
 }
