77e3ee30df7daf5e54d144bfe632faf6bb51b161
Drew Coleman <dcolem12@my.westga.edu>
2018-06-12 09:38:32 -0400

    Fixed issue where computerPlayer was polite and never win - the isGameOver check was outside the looping

--- pig01/src/edu/westga/cs6910/pig/view/ComputerPane.java
+++ pig01/src/edu/westga/cs6910/pig/view/ComputerPane.java
@@ -103,11 +103,9 @@ public class ComputerPane extends GridPane implements InvalidationListener {
 		 */
 		@Override
 		public void handle(ActionEvent arg0) {
-			ComputerPane.this.theComputer.setMaximumRolls(2);
 			if (!ComputerPane.this.theGame.isGameOver()) {
+				ComputerPane.this.theComputer.setMaximumRolls(2);
 				ComputerPane.this.theComputer.takeTurn();
-			}
-			if (!ComputerPane.this.theGame.isGameOver()) {
 				ComputerPane.this.theGame.hold();
 			}
 		}
