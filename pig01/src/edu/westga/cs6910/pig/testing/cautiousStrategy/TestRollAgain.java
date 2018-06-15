93e051882a45316cdca6d8e36197a19a1df978e6
Drew Coleman <dcolem12@my.westga.edu>
2018-06-15 12:52:41 -0400

    Added three tests which check the RollAgain method in CautiousStrategy tested number of times rolled, value of those rolls and difference all the results returned their expected value

---
+++ pig01/src/edu/westga/cs6910/pig/testing/cautiousStrategy/TestRollAgain.java
@@ -0,0 +1,38 @@
+package edu.westga.cs6910.pig.testing.cautiousStrategy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.strategies.CautiousStrategy;
+
+class TestRollAgain {
+
+	/**
+	 * This test insure that the rollAgain returns true when presented with 2 for number of times rolled
+	 */
+	@Test
+	void testCautiousStrategyRollAgainAfterRolling2TimesExpectTrue() {
+		CautiousStrategy careful = new CautiousStrategy();
+		assertEquals(true, careful.rollAgain(2, 5, 99));
+	}
+	
+	/**
+	 * This test insure that the rollAgain returns true when presented with 2 for number of times rolled
+	 */
+	@Test
+	void testCautiousStrategyRollAgainAfterAmountGainedIs10TimesExpectTrue() {
+		CautiousStrategy careful = new CautiousStrategy();
+		assertEquals(true, careful.rollAgain(1, 10, 99));
+	}
+	
+	/**
+	 * This test insure that the rollAgain returns false when presented with under 2 and less than 10 
+	 */
+	@Test
+	void testCautiousStrategyRollAgainShouldNotTriggerExpectFalse() {
+		CautiousStrategy careful = new CautiousStrategy();
+		assertEquals(false, careful.rollAgain(1, 8, 1));
+	}
+
+}
